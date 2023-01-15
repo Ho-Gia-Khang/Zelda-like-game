c17a7aa68110b2ec0812fc8c128773334b1fbcce
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-01-15 11:01:29 +0700

    created the interaction between the player and the enemies
    the player can now kill the enemy, the enemy can damage the player, and both the enemies and the player are now flickering when they are damaged

--- src/player.py
+++ src/player.py
@@ -45,6 +45,11 @@ class Player(Entity):
         self.exp = 123
         self.speed = self.stats['speed']
 
+        # damage timer
+        self.vulnerable = True
+        self.hurt_time = None
+        self.invulnerability_duration = 500
+
     def import_player_assets(self):
         character_path = '../graphics/player/'
         self.animations = {'up' : [], 'down' : [], 'left' : [], 'right' : [],
@@ -156,7 +161,7 @@ class Player(Entity):
         current_time = pygame.time.get_ticks()
 
         if self.attacking:
-            if current_time - self.attack_time >= self.attack_cooldown:
+            if current_time - self.attack_time >= self.attack_cooldown + weapon_data[self.weapon]['cooldown']:
                 self.attacking = False
                 self.destroy_attack()
 
@@ -167,6 +172,10 @@ class Player(Entity):
         if not self.can_switch_magic:
             if current_time - self.magic_switch_time >= self.magic_switch_cooldown:
                 self.can_switch_magic = True
+
+        if not self.vulnerable:
+            if current_time - self.hurt_time >= self.invulnerability_duration:
+                self.vulnerable = True
 
     def animate(self):
         animation = self.animations[self.status]
@@ -179,6 +188,18 @@ class Player(Entity):
         # set the image
         self.image = animation[int(self.frame_index)]
         self.rect = self.image.get_rect(center = self.hitbox.center)
+
+        # flicker
+        if not self.vulnerable:
+            alpha = self.wave_value()
+            self.image.set_alpha(alpha)
+        else:
+            self.image.set_alpha(255)
+
+    def get_full_weapon_damage(self):
+        base_dmg = self.stats['attack']
+        weapon_dmg = weapon_data[self.weapon]['damage']
+        return base_dmg + weapon_dmg
 
     def update(self):
         self.input()
--- src/weapon.py
+++ src/weapon.py
@@ -3,8 +3,8 @@ import pygame
 class Weapon(pygame.sprite.Sprite):
     def __init__(self, player, groups):
         super().__init__(groups)
+        self.sprite_type = 'weapon'
         direction = player.status.split('_')[0]
-
 
         # graphic
         full_path = f'../graphics/weapons/{player.weapon}/{direction}.png'

ff9f3d34b390f4e0736d5b1e9d11d488cfea7455
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-01-17 20:16:13 +0700

    add the upgrade system
    including the uograde mechanic and the gui for the upgrade system

--- src/player.py
+++ src/player.py
@@ -39,11 +39,12 @@ class Player(Entity):
         self.magic_switch_cooldown = 200
 
         # stats
-        self.stats = {'health' : 100, 'energy' : 60, 'attack' : 10, 'magic' : 4, 'speed' : 6}
+        self.stats = {'health' : 100, 'energy' : 60, 'attack' : 10, 'magic' : 4, 'speed' : 5}
+        self.max_stats = {'health' : 300, 'energy' : 140, 'attack' : 20, 'magic' : 10, 'speed' : 10}
+        self.upgrade_cost = {'health' : 100, 'energy' : 100, 'attack' : 100, 'magic' : 100, 'speed' : 100}
         self.health = self.stats['health']
         self.energy = self.stats['energy']
-        self.exp = 123
-        self.speed = self.stats['speed']
+        self.exp = 500
 
         # damage timer
         self.vulnerable = True
@@ -191,6 +192,12 @@ class Player(Entity):
         magic_dmg = magic_data[self.magic]['strength']
         return base_dmg + magic_dmg
 
+    def get_value_by_index(self, index):
+        return list(self.stats.values())[index]
+
+    def get_cost_by_index(self, index):
+        return list(self.upgrade_cost.values())[index]
+
     def energy_recovery(self):
         if self.energy < self.stats['energy']:
             self.energy += 0.01 * self.stats['magic']
@@ -202,5 +209,5 @@ class Player(Entity):
         self.cooldowns()
         self.get_status()
         self.animate()
-        self.move(self.speed)
+        self.move(self.stats['speed'])
         self.energy_recovery()
